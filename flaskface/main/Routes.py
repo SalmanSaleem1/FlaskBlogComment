33454174ba70463524fc93ba99d55d15bf029105
SalmanSaleem1 <salman.saleem@southvillesol.com>
2019-05-09 16:24:42 +0500

    Add quotes

--- flaskface/main/Routes.py
+++ flaskface/main/Routes.py
@@ -9,5 +9,5 @@ main = Blueprint('main', __name__)
 @login_required
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.create_at.desc()).paginate(page=page, per_page=5)
-    return render_template('Home.html', posts=posts, title='Home')
+    posts = Post.query.order_by(Post.create_at.desc()).paginate(page=page, per_page=5, error_out=False)
+    return render_template('Home.html', title='Home', posts=posts)
