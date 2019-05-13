758cd7fef18d060b8630b3eeb55d5b1d49cd4000
SalmanSaleem1 <salman.saleem@southvillesol.com>
2019-05-13 23:54:43 +0500

    add follow privelliges

--- flaskface/main/Routes.py
+++ flaskface/main/Routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request
 from flask_login import login_required
 from flaskface.Models import Post
+from flask_login import current_user
 
 main = Blueprint('main', __name__)
 
@@ -9,5 +10,6 @@ main = Blueprint('main', __name__)
 @login_required
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.create_at.desc()).paginate(page=page, per_page=5, error_out=False)
+    # posts = Post.query.order_by(Post.create_at.desc()).paginate(page=page, per_page=5, error_out=False)
+    posts = current_user.followed_posts().all()
     return render_template('Home.html', title='Home', posts=posts)
